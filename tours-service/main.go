121d9e8aca9a9a14e4bb2777545c4b8b8277453b
IvanPartalo <ivanpartalo123@gmail.com>
2024-03-05 20:36:13 +0100

    go template service

---
+++ tours-service/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database-example/handler"
+	"database-example/model"
+	"database-example/repo"
+	"database-example/service"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func initDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=super dbname=gorm port=5432 sslmode=disable"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		print(err)
+		return nil
+	}
+
+	database.AutoMigrate(&model.Student{})
+	//database.Exec("INSERT INTO students VALUES ('aec7e123-233d-4a09-a289-75308ea5b7e6', 'Marko Markovic', 'Graficki dizajn')")
+	return database
+}
+
+func startServer(handler *handler.StudentHandler) {
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/students/{id}", handler.Get).Methods("GET")
+	router.HandleFunc("/students", handler.Create).Methods("POST")
+
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
+	println("Server starting")
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
+
+func main() {
+	database := initDB()
+	if database == nil {
+		print("FAILED TO CONNECT TO DB")
+		return
+	}
+	repo := &repo.StudentRepository{DatabaseConnection: database}
+	service := &service.StudentService{StudentRepo: repo}
+	handler := &handler.StudentHandler{StudentService: service}
+
+	startServer(handler)
+}
